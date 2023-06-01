0970953157160ccb3e3257cfc563ddc724cfb4b5
120 <xelloss120@hotmail.com>
2023-06-01 23:59:54 +0900

    規定デバイスの周波数スペクトル表示まで試作

---
+++ Assets/main.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using TMPro;
+
+public class main : MonoBehaviour
+{
+    [SerializeField] TMP_InputField LengthSec;
+    [SerializeField] TMP_InputField Frequency;
+    [SerializeField] AudioSource AudioSource;
+    [SerializeField] LineRenderer LineRenderer;
+    
+    const int LEN = 64;
+    float[] Spectrum = new float[LEN];
+
+    void Start()
+    {
+        int lengthSec;
+        int frequency;
+        
+        int.TryParse(LengthSec.text, out lengthSec);
+        int.TryParse(Frequency.text, out frequency);
+
+        AudioSource.clip = Microphone.Start(null, true, lengthSec, frequency);
+        while (Microphone.GetPosition(null) <= 0) { }
+        AudioSource.Play();
+
+        for (int i = 0; i < LEN; i++)
+        {
+            var vec3 = new Vector3(-0.5f + (1.0f / LEN * i), 0, 0);
+            LineRenderer.SetPosition(i, vec3);
+        }
+    }
+
+    void Update()
+    {
+        AudioSource.GetSpectrumData(Spectrum, 0, FFTWindow.Rectangular);
+        for (int i = 0; i < LEN; i++)
+        {
+            var vec3 = LineRenderer.GetPosition(i);
+            vec3.y = Spectrum[i] * 3;
+            LineRenderer.SetPosition(i, vec3);
+        }
+    }
+}
